17a6efc22d4af0f71673291a064f58a5e2b94dd6
bofm <bofm.gh@gmail.com>
2019-09-02 13:14:10 +0300

    up arrow

--- keyboards/jiran/keymaps/bofm/keymap.c
+++ keyboards/jiran/keymaps/bofm/keymap.c
@@ -67,7 +67,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   // └────────┼────────┼────────┼────────┼────────┼────────┼────────┤       ├────────┼────────┼────────┼────────┼────────┼────────┼────────┘
                KC_LSFT, KC_P1,   KC_P2,   KC_P3,   KC_PMNS, KC_PGDN,         KC_MUTE, KC_LEFT, KC_DOWN, KC_RGHT, KC_SCLN, KC_RCTL,
   //          ├────────┼────────┼────────┼────────┼────────┼────────┤       ├────────┼────────┼────────┼────────┼────────┼────────┤
-               KC_LCTL,  KC_Z,    KC_P0,   KC_PDOT, KC_PPLS, KC_END,          KC_VOLD, KC_M,    _______, KC_DOT,  KC_BSLS, KC_RCTL,
+               KC_LCTL,  KC_Z,    KC_P0,   KC_PDOT, KC_PPLS, KC_END,          KC_VOLD, KC_M,    XXXXXXX, KC_DOT,  KC_BSLS, KC_RCTL,
   //          └────────┴────────┴────────┴────────┼────────┼────────┤       ├────────┼────────┴────────┴────────┴────────┴────────┘
                                           _______, KC_LALSPC,KC_ESC,          KC_ENT,KC_RALDEL, _______
                                     //   └────────┴────────┴────────┘       └────────┴────────┴────────┘
@@ -77,11 +77,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //          ┌────────┬────────┬────────┬────────┬────────┬────────┐       ┌────────┬────────┬────────┬────────┬────────┬────────┐
                KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,           KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_EQL,
   // ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤       ├────────┼────────┼────────┼────────┼────────┼────────┼────────┐
-      KC_CMD,  KC_CAPS, KC_Q,    KC_W,    KC_E,    KC_F11,  KC_F12,          KC_VOLU, KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RCMD,
+      KC_CMD,  KC_CAPS, KC_Q,    KC_W,    KC_E,    KC_F11,  KC_F12,          KC_VOLU, KC_U,    KC_UP,    KC_O,    KC_P,    KC_LBRC, KC_RCMD,
   // └────────┼────────┼────────┼────────┼────────┼────────┼────────┤       ├────────┼────────┼────────┼────────┼────────┼────────┼────────┘
-               KC_LSFT, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,            KC_MUTE, KC_LEFT, KC_UP,   KC_RGHT, KC_SCLN, KC_RCTL,
+               KC_LSFT, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,            KC_MUTE, KC_LEFT, KC_DOWN,  KC_RGHT, KC_SCLN, KC_RCTL,
   //          ├────────┼────────┼────────┼────────┼────────┼────────┤       ├────────┼────────┼────────┼────────┼────────┼────────┤
-               KC_LCTL,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,            KC_VOLD, KC_M,    KC_DOWN, KC_DOT,  KC_BSLS, KC_RCTL,
+               KC_LCTL,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,            KC_VOLD, KC_M,    XXXXXXX, KC_DOT,  KC_BSLS, KC_RCTL,
   //          └────────┴────────┴────────┴────────┼────────┼────────┤       ├────────┼────────┴────────┴────────┴────────┘────────┘
                                           ADJUST,  KC_SPC,  KC_ESC,          KC_ENT,  KC_BSPC, _______
                                     //   └────────┴────────┴────────┘       └────────┴────────┴────────┘
