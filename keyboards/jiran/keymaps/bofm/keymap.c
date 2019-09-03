0a9f1738f923746187ae559e642216ab4cecc648
bofm <bofm.gh@gmail.com>
2019-09-03 15:18:37 +0300

    mm

--- keyboards/jiran/keymaps/bofm/keymap.c
+++ keyboards/jiran/keymaps/bofm/keymap.c
@@ -43,6 +43,7 @@ enum custom_keycodes {
 #define KC_CMD_L LGUI(KC_LEFT)
 #define KC_CMD_R LGUI(KC_RIGHT)
 #define KC_CMDSP LGUI(KC_SPC)
+#define KC_CMDSPT LGUI_T(LGUI(KC_SPC))
 
 ////////////////////////////////////////
 
@@ -52,27 +53,27 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //          ┌────────┬────────┬────────┬────────┬────────┬────────┐       ┌────────┬────────┬────────┬────────┬────────┬────────┐
                KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,            KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,
   // ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤       ├────────┼────────┼────────┼────────┼────────┼────────┼────────┐
-      KC_CMDSP, KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,            KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC,
+      KC_CMDSP,KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,            KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC,
   // └────────┼────────┼────────┼────────┼────────┼────────┼────────┤       ├────────┼────────┼────────┼────────┼────────┼────────┼────────┘
                KC_LSFT, KC_A,    KC_S,    KC_D,    KC_LOF,  KC_G,            KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_RSHQT,
   //          ├────────┼────────┼────────┼────────┼────────┼────────┤       ├────────┼────────┼────────┼────────┼────────┼────────┤
                KC_LCTL,  KC_Z,    KC_X,   KC_SSSC,  KC_V,    KC_B,            KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RCTL,
   //          └────────┴────────┴────────┴────────┼────────┼────────┤       ├────────┼────────┼────────┴────────┴────────┴────────┘
-                                          KC_CMD, KC_LALSPC,KC_LOESC,        KC_LOENT,KC_RALBSP, RAISE
+                                           KC_CMD,KC_LALSPC,KC_LOESC,        KC_LOENT,KC_RALBSP, RAISE
                                   //     └────────┴────────┴────────┘       └────────┴────────┴────────┘
   ),
 
   [_LOWER] = LAYOUT(
   //          ┌────────┬────────┬────────┬────────┬────────┬────────┐       ┌────────┬────────┬────────┬────────┬────────┬────────┐
-               KC_GRV,  KC_P7,   KC_P8,   KC_P9,   KC_PSLS, KC_HOME,         KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_EQL,
+               KC_GRV,   KC_F1,   KC_F2,    KC_F3,   KC_F4,  KC_F5,           KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_EQL,
   // ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤       ├────────┼────────┼────────┼────────┼────────┼────────┼────────┐
-      KC_CMD,  KC_NLCK, KC_P4,   KC_P5,   KC_P6,   KC_PAST, KC_PGUP,         KC_VOLU, KC_ALT_L, KC_UP,   KC_ALT_R, KC_P,    KC_LBRC, KC_RCMD,
+       KC_ESC, KC_NLCK, _______, _______,  _______, _______, _______,         KC_VOLU, KC_ALT_L, KC_UP,   KC_ALT_R, KC_P,  KC_LBRC, KC_RCMD,
   // └────────┼────────┼────────┼────────┼────────┼────────┼────────┤       ├────────┼────────┼────────┼────────┼────────┼────────┼────────┘
-               KC_LSFT, KC_P1,   KC_P2,   KC_P3,   KC_PMNS, KC_PGDN,         KC_MUTE, KC_LEFT, KC_DOWN, KC_RGHT, KC_SCLN, KC_RCTL,
+               KC_LSFT, _______, _______, _______,  _______, _______,         KC_MUTE, KC_LEFT, KC_DOWN, KC_RGHT, KC_SCLN, KC_RCTL,
   //          ├────────┼────────┼────────┼────────┼────────┼────────┤       ├────────┼────────┼────────┼────────┼────────┼────────┤
-               KC_LCTL,  KC_Z,    KC_P0,   KC_PDOT, KC_PPLS, KC_END,          KC_VOLD,KC_CMD_L,XXXXXXX, KC_CMD_R, KC_BSLS, KC_RCTL,
+               KC_LCTL,  _______, _______, _______, _______, _______,         KC_VOLD,KC_CMD_L,XXXXXXX, KC_CMD_R, KC_BSLS, KC_RCTL,
   //          └────────┴────────┴────────┴────────┼────────┼────────┤       ├────────┼────────┴────────┴────────┴────────┴────────┘
-                                          _______, KC_LALSPC,KC_ESC,          KC_ENT,KC_RALDEL, _______
+                                           KC_CMD, KC_LALSPC,KC_ESC,         KC_ENT,KC_RALDEL, _______
                                     //   └────────┴────────┴────────┘       └────────┴────────┴────────┘
   ),
 
