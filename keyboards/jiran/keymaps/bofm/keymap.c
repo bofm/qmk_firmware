05cbe17b070bbd31b14d08e8a8464f02364eaaa4
bofm <bofm.gh@gmail.com>
2019-09-09 18:42:23 +0300

    m

--- keyboards/jiran/keymaps/bofm/keymap.c
+++ keyboards/jiran/keymaps/bofm/keymap.c
@@ -27,6 +27,7 @@ enum custom_keycodes {
 #define KC_RALBSP RALT_T(KC_BSPC)
 #define KC_ALDEL RALT_T(KC_DEL)
 #define KC_LSHSPC LSFT_T(KC_SPC)
+#define KC_LSEQ LSFT_T(KC_EQL)
 #define KC_RSHBSP RSFT_T(KC_BSPC)
 #define KC_RSHDEL RSFT_T(KC_DEL)
 #define KC_RALDEL RALT_T(KC_DEL)
@@ -47,6 +48,7 @@ enum custom_keycodes {
 #define KC_LCBR LSFT(KC_LBRC)
 #define KC_RCBR LSFT(KC_RBRC)
 #define KC_PLUS LSFT(KC_EQL)
+#define KC_MULT LSFT(KC_8)
 #define KC_RAIDEL LT(_RAISE, KC_DEL)
 
 ////////////////////////////////////////
@@ -71,9 +73,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //          ┌────────┬────────┬────────┬────────┬────────┬────────┐       ┌────────┬────────┬────────┬────────┬────────┬────────┐
                KC_GRV,   KC_F1,   KC_F2,    KC_F3,   KC_F4,  KC_F5,           KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_EQL,
   // ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤       ├────────┼────────┼────────┼────────┼────────┼────────┼────────┐
-       KC_ESC, KC_NLCK, _______, _______, KC_LBRC, KC_RBRC, KC_PLUS,         KC_VOLU, KC_ALT_L, KC_UP, KC_ALT_R,KC_1PASS, KC_LBRC, KC_RCMD,
+       KC_ESC, KC_NLCK, _______, _______, KC_LBRC, KC_RBRC,  KC_EQL,         KC_VOLU, KC_ALT_L, KC_UP, KC_ALT_R,KC_1PASS, KC_LBRC, KC_RCMD,
   // └────────┼────────┼────────┼────────┼────────┼────────┼────────┤       ├────────┼────────┼────────┼────────┼────────┼────────┼────────┘
-               KC_LSFT, _______, _______, KC_LPRN, KC_RPRN,  KC_EQL,         KC_MUTE, KC_LEFT, KC_DOWN, KC_RGHT, KC_SCLN, KC_RCTL,
+               KC_LSFT, _______, _______, KC_LPRN, KC_RPRN, KC_PLUS,         KC_MUTE, KC_LEFT, KC_DOWN, KC_RGHT, KC_SCLN, KC_RCTL,
   //          ├────────┼────────┼────────┼────────┼────────┼────────┤       ├────────┼────────┼────────┼────────┼────────┼────────┤
                KC_LCTL,  _______,_______, KC_LCBR, KC_RCBR, _______,         KC_VOLD, KC_CMD_L,XXXXXXX, KC_CMD_R, KC_BSLS, KC_RCTL,
   //          └────────┴────────┴────────┴────────┼────────┼────────┤       ├────────┼────────┴────────┴────────┴────────┴────────┘
@@ -85,11 +87,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //          ┌────────┬────────┬────────┬────────┬────────┬────────┐       ┌────────┬────────┬────────┬────────┬────────┬────────┐
                KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,           KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_EQL,
   // ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤       ├────────┼────────┼────────┼────────┼────────┼────────┼────────┐
-      KC_CMD,  KC_CAPS, KC_Q,    KC_W,    KC_E,    KC_F11,  KC_F12,          KC_VOLU, KC_U,    KC_UP,    KC_O,    KC_P,    KC_LBRC, KC_RCMD,
+      KC_CMD,  KC_CAPS, KC_SLSH,  KC_7,    KC_8,    KC_9,   KC_MINS,         KC_VOLU, KC_U,    KC_UP,    KC_O,    KC_P,    KC_LBRC, KC_RCMD,
   // └────────┼────────┼────────┼────────┼────────┼────────┼────────┤       ├────────┼────────┼────────┼────────┼────────┼────────┼────────┘
-               KC_LSFT, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,            KC_MUTE, KC_LEFT, KC_DOWN,  KC_RGHT, KC_SCLN, KC_RCTL,
+               KC_LSEQ, KC_MULT,  KC_4,    KC_5,    KC_6,   KC_PLUS,           KC_MUTE, KC_LEFT, KC_DOWN,  KC_RGHT, KC_SCLN, KC_RCTL,
   //          ├────────┼────────┼────────┼────────┼────────┼────────┤       ├────────┼────────┼────────┼────────┼────────┼────────┤
-               KC_LCTL,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,            KC_VOLD, KC_M,    XXXXXXX, KC_DOT,  KC_BSLS, KC_RCTL,
+               KC_LCTL,  KC_0,    KC_1,    KC_2,    KC_3,   KC_DOT,           KC_VOLD, KC_M,    XXXXXXX, KC_DOT,  KC_BSLS, KC_RCTL,
   //          └────────┴────────┴────────┴────────┼────────┼────────┤       ├────────┼────────┴────────┴────────┴────────┘────────┘
                                           ADJUST,  KC_CMD,  KC_SPC,          KC_ENT,  KC_BSPC, _______
                                     //   └────────┴────────┴────────┘       └────────┴────────┴────────┘
